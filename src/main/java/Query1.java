1a40e37dbf003ded8078d1c627f60f81b46df747
andrea <conidi.andrea7@gmail.com>
2020-05-09 10:30:02 +0200

    Query 1 versione 2, manca ancora la parte sui tamponi e count dei giorni.

--- src/main/java/Query1.java
+++ src/main/java/Query1.java
@@ -2,17 +2,22 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.Optional;
+import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
 import scala.Tuple3;
 import utility.Outlet;
 import utility.OutletParser;
 
+import java.text.ParseException;
 import java.time.LocalDate;
-import java.util.Date;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.WeekFields;
+import java.util.Locale;
 
 public class Query1 {
 
-    private static String pathToFile = "src/dataset/covid19datinazionali.csv";
+    private static final String pathToFile = "src/dataset/covid19datinazionali.csv";
 
     public static void main(String[] args) {
 
@@ -24,62 +29,70 @@ public class Query1 {
         String firstRow = logLines.first();
         JavaRDD<String> covid1 = logLines.filter(x -> !x.equals(firstRow));
 
-
+        /*
+         Parsato il csv
+        */
         JavaRDD<Outlet> outlets =
                 covid1.map(
                         line -> OutletParser.parseCSV(line));
 
+        /*
+         Creazione di due RDD: un RDD normale e uno contentente i dati shiftati di un giorno
+        */
+        JavaPairRDD<LocalDate,Integer> pairRDD_healed= outlets.
+                mapToPair(x -> new Tuple2<>(x.getDateTime(),x.getGuariti()));
+        JavaPairRDD<LocalDate,Integer> pairRDD_healed_shifted= outlets.
+                mapToPair(x ->new Tuple2<>(x.getDateTime().plusDays(1), x.getGuariti()));
 
-        JavaRDD<Tuple3<String, Integer, Integer>> result = outlets.map(x -> new Tuple3<String, Integer, Integer>
-                (x.getWeek(), x.getGuariti(), x.getTamponi()));
+        /*
+         Creato RDD contentente la coppia chiave,valore contentente valori non cumulativi.
+         */
+        JavaPairRDD<LocalDate, Integer> pairRDD_daily_healed = pairRDD_healed_shifted.
+                rightOuterJoin(pairRDD_healed).
+                mapToPair(new PairFunction<Tuple2<LocalDate, Tuple2<Optional<Integer>, Integer>>, LocalDate, Integer>() {
+                    @Override
+                    public Tuple2<LocalDate, Integer> call(Tuple2<LocalDate, Tuple2<Optional<Integer>, Integer>> row) throws Exception {
+                        if(row._2()._1().isPresent()) {
+                            Integer diff=row._2()._2() - row._2()._1().get();
+                            return new Tuple2<>(row._1(),diff);
+                        }
+                        return new Tuple2<>(row._1(),row._2()._2());
+                    }
+                });
 
-        /*String secondRow = covid1.first();
+        /*
+         Creazione di un RDD (map reduce) contentente la somma dei valori per settimana
+        */
+        JavaPairRDD<String,Integer> pairRDD_week_healed=pairRDD_daily_healed.
+                mapToPair(x-> new Tuple2<>(getWeek(x._1().toString()),x._2())).
+                reduceByKey((a,b)->a+b);
+        //TODO: essere robusti sul numero di giorni per settimana, non è detto che sia sempre diviso 7
+        /*
+         Creazione di un RDD contentente la media settimanale dei guariti
+         */
+        JavaPairRDD<String,Double> pairRDD_avg_healed=pairRDD_week_healed.
+                mapToPair(x-> new Tuple2<>(x._1(),(double)x._2()/7));
 
-        JavaRDD<String> covid2 = covid1.filter(x -> !x.equals(secondRow));
-        JavaRDD<Outlet> outlet2 =
-                covid2.map(
-                        line -> OutletParser.parseCSV(line));
-        JavaRDD<Tuple3<String, Integer, Integer>> result2 = outlet2.map(x -> new Tuple3<String, Integer, Integer>
-                (x.getWeek(), x.getGuariti(), x.getTamponi()));*/
-
-
-
-        /*//per pulire l'RDD dai dati cumulativi
-        JavaRDD<Tuple3<LocalDate, Integer, Integer>> result11 = outlets.map(x -> new Tuple3<LocalDate, Integer, Integer>
-                (x.getDateTime(), x.getGuariti(), x.getTamponi()));
-        JavaRDD<Tuple3<LocalDate, Integer, Integer>> result3 = outlets.map(x -> new Tuple3<LocalDate, Integer, Integer>
-                (x.getDateTime().minusDays(1), x.getGuariti(), x.getTamponi()));*/
-
-        JavaPairRDD<LocalDate,Integer> result5= outlets.mapToPair(x -> new Tuple2<>(x.getDateTime(),x.getGuariti()));
-        JavaPairRDD<LocalDate,Integer> result4= outlets.mapToPair(x ->new Tuple2<>(x.getDateTime().plusDays(1), x.getGuariti()));
-        JavaPairRDD<LocalDate,Integer>result6=result5.union(result4).reduceByKey((a,b) -> Math.abs(b-a)).filter(x-> !x._1().toString().equals("2020-05-07")).sortByKey();
-         JavaPairRDD<LocalDate, String> result7= outlets.mapToPair(x -> new Tuple2<>(x.getDateTime(),x.getWeek()));
-         JavaPairRDD<LocalDate, Tuple2<Integer, String>> result8= result6.join(result7).sortByKey();
-         JavaRDD<Tuple2<Integer, String>> result9= result8.map(x->x._2());
-        JavaPairRDD<String, Integer> prova_media = result9.mapToPair(x -> new Tuple2<>(x._2(), x._1())).reduceByKey((a, b) -> a + b).sortByKey();
-        Integer average;
-
-        for (Tuple2<String, Integer> i: prova_media.collect()){
-            average=i._2()/7;
-            System.out.println("settimana:  "+i._1()+" media: "+average);
-
-        }
-
-
-        /*for (Tuple2<Integer, String> i: result9.collect()){
-            System.out.println(i);
-
-        }*/
-
-
-      /*  for (Tuple2<LocalDate, Tuple2<Integer, String>> i: result8.collect()){
-            System.out.println(i);
-
-        }*/
-
-
-            sc.stop();
-
+        for (Tuple2<String, Double> avg: pairRDD_avg_healed.collect()){
+            System.out.println(avg);
 
         }
+        sc.stop();
+       }
+
+
+
+    public static String getWeek(String data) throws ParseException {
+
+        System.out.println(data);
+        //2020-02-24T18:00:00
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.ITALIAN);
+        LocalDate date = LocalDate.parse(data, df);
+        WeekFields weekFields = WeekFields.of(Locale.getDefault());
+        int week = date.get(weekFields.weekOfWeekBasedYear());
+        String w= "W"+week;
+
+
+        return w;
     }
+}
